69356d90aec80ca14d95add9562d4f0082279e70
wenhan <wenhan.work@gmail.com>
2019-07-19 00:32:21 +0800

    feat: Request data transform
    Add a function to transform object data to string.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types';
 import xhr from './xhr';
 import { buildUrl } from './helpers/url';
+import { transformRequest } from './helpers/data';
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config);
@@ -9,11 +10,17 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config);
+  config.data = transformRequestData(config);
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config;
   return buildUrl(url, params);
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  const { data } = config;
+  return transformRequest(data);
 }
 
 export default axios;
